3269cf420fb2eb14c04f3f0d4282dd02cf2446de
Kola771 <koladeaboudou@gmail.com>
2023-07-21 19:44:26 +0200

    test

--- zip.php
+++ zip.php
@@ -25,3 +25,17 @@ if ($zip->open($zipName, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true) {
 } else {
     echo 'Échec lors de la création de l\'archive.';
 }
+$file = "essai.html";
+$op = fopen($file, 'w');
+fwrite($op, "<p>Hola</p>");
+fwrite($op, "\n<p>Hola</p>");
+$t = file($file);
+var_dump($t);
+foreach($t as $key => $val)
+{
+    echo htmlspecialchars($val);
+}
+
+// $open = fopen('exemple.txt', 'w');
+$open = file('exemple.txt');
+var_dump($open) ;
